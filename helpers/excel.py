e02cea7af8b6e4b50f6f7e12805a3740ef68d474
tacos <tacos>
2021-09-28 16:50:08 -0600

    comment

--- helpers/excel.py
+++ helpers/excel.py
@@ -1,4 +1,5 @@
 import pandas as pd
+# unused import but used to gen requirements.txt
 import openpyxl
 
 def update_balance(balance=0, time=0, usd_balance=0):
@@ -8,10 +9,8 @@ def update_balance(balance=0, time=0, usd_balance=0):
         'Balance': [1.88112],
         'USD': ['100007344']
         })
-    df2 = pd.read_excel(path, sheet_name='Wallet Tracking')
+    df2 = pd.read_excel(path, sheet_name='wallet_tracking')
     res = pd.concat([df2, df1])
     writer = pd.ExcelWriter(path, engine='openpyxl', mode='a', if_sheet_exists='replace')
-    res.to_excel(writer, sheet_name='Wallet Tracking', index=False)
+    res.to_excel(writer, sheet_name='wallet_tracking', index=False)
     writer.save()
-
-update_balance()
