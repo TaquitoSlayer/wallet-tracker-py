6cf5d7427c07e3b093f1c3a2850ead81a05bd5a7
tacos <tacos>
2021-09-28 12:52:48 -0600

    balance fetch done

--- helpers/etherscan.py
+++ helpers/etherscan.py
@@ -1,1 +1,17 @@
 import requests
+import logging
+from web3 import Web3
+from decimal import Decimal
+
+def get_balance(wallet, api_key='YourApiKeyToken'):
+    query = f'https://api.etherscan.io/api?module=account&action=balance&address={wallet}&tag=latest&apikey={api_key}'
+
+    resp = requests.get(query)
+
+    try:
+        balance = resp.json()['result']
+        balance_ether = Web3.fromWei(Decimal(balance), 'ether')
+        print('CURRENT BALANCE:', balance_ether)
+    except Exception as e:
+        logging.info(e)
+        pass
