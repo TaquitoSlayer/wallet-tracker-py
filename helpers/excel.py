fd8d204bc98403cf83171f6e294de584fa8db4e8
tacos <tacos>
2021-09-28 16:49:15 -0600

    use pandas to add to xlsx

---
+++ helpers/excel.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import openpyxl
+
+def update_balance(balance=0, time=0, usd_balance=0):
+    path = './../balance.xlsx'
+    df1 = pd.DataFrame({
+        'Time': ['Timetest12314'],
+        'Balance': [1.88112],
+        'USD': ['100007344']
+        })
+    df2 = pd.read_excel(path, sheet_name='Wallet Tracking')
+    res = pd.concat([df2, df1])
+    writer = pd.ExcelWriter(path, engine='openpyxl', mode='a', if_sheet_exists='replace')
+    res.to_excel(writer, sheet_name='Wallet Tracking', index=False)
+    writer.save()
+
+update_balance()
